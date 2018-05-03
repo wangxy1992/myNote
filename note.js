86238ab078cfa8d93b3bd8d110f6514bee7424e6
wxy_Wang@87money.com <wxy_Wang@87money.com>
2018-05-03 15:02:11 +0800

    2

--- note.js
+++ note.js
@@ -1,6 +1,10 @@
+// 1:图片压缩上传
+// 2.微信分享
+
 
 // 一、图片压缩上传
-<input type="file" accept="image/*" class="info-file-none" ng-model="avatar.value" onchange="angular.element(this).scope().avatar.change(this.files)" style="right:0;left: auto;z-index: 9">
+<input type="file" accept="image/*" class="info-file-none" ng-model="avatar.value"
+onchange="angular.element(this).scope().avatar.change(this.files)"style="right:0;left: auto;z-index: 9">
 
 $scope.avatar = {
     title: i18n.AVATAR,
@@ -87,6 +91,114 @@ imageCompress: function (file, callback) {
         var re = this.result;
         canvasDataURL(re, percent);
     }
-
     // return retImgStr;
+}
+
+//二、 微信分享
+wxShare: function () { //调用改函数时传的参数，以arguments的形式进行获取
+    // 分析网址
+    var reg1 = /^.*\/wxsrc\/.*$/;
+    var reg2 = constant.urlReg;
+    var currentUrl = location.href;
+    var urlResult = reg2.exec(currentUrl);
+    var replaceUrl = reg1.test(currentUrl) ? 'wxsrc' : 'wx';
+    if (urlResult != null) {
+        var sginBaseUrl = urlResult[0];
+    }
+    var shareTitle = "「涨听」-一问就有财";
+    var shareDesc = "一个专业的付费投资知识分享平台，在涨听上你可以向各路大牛一对一付费问财，并听到他们的亲口回答，解决你的投资困惑";
+    var shareImage = require('../../assets/style/images/icon/logo_share.png');
+    shareImage = `${constant.BASE_URL.slice(0,-1)}${shareImage}`;
+    var shareObj = constant.shareObjDefault;
+    var linkUrl = constant.BASE_URL + "weixin/subscribedCheck?uri=" + replaceUrl + "/index.html#!" + arguments[0];
+    shareObj.link = linkUrl.replace("#", "%23");
+    if (arguments.length == 1) {
+        // shareObj.link = arguments[0];
+        shareObj.title = shareTitle;
+        shareObj.desc = shareDesc;
+        shareObj.imgUrl = shareImage;
+    }
+    else if (arguments.length == 3) {
+        // shareObj.link = arguments[0];
+        shareObj.title = arguments[1];
+        shareObj.desc = arguments[2];
+        shareObj.imgUrl = shareImage;
+    }
+    else if (arguments.length == 4) {
+        // shareObj.link = arguments[0];
+        shareObj.title = arguments[1];
+        shareObj.desc = arguments[2];
+        shareObj.imgUrl = arguments[3];
+        shareObj.imgUrl = this.transShareImgDomain(shareObj.imgUrl);
+    }
+    shareObj.success = function () { };
+
+    var signUrl = constant.BASE_URL + "weixin/signPackage";
+    var urlParams = {
+        // 向后台传递当前url
+        url: constant.BASE_URL.split("//")[1] + sginBaseUrl
+    };
+    $.ajax({
+        type: "POST",
+        url: signUrl,
+        data: urlParams,
+        dataType: "json",
+        success: function (data) {
+            if (data.status == 0) {
+                wx.config({
+                    // debug: true,
+                    appId: data.data.signPackage.appId,
+                    timestamp: data.data.signPackage.timestamp,
+                    nonceStr: data.data.signPackage.nonceStr,
+                    signature: data.data.signPackage.signature,
+                    jsApiList: constant.jsApiList
+                });
+                wx.ready(function () {
+                    wx.onMenuShareAppMessage(shareObj);
+                    wx.onMenuShareTimeline(shareObj);
+                    $('.opinion-detail-content img').click(function (event) {
+                        var reg = /http|https/gi;
+                        var imageArray = [];
+                        var curImageUrl = $(this).attr('src');
+                        if (!reg.test(curImageUrl)) {
+                            curImageUrl = constant.BASE_URL + curImageUrl;
+                        }
+                        imageArray.push(curImageUrl);
+                        wx.previewImage({
+                            urls: imageArray,
+                            current: curImageUrl
+                        });
+                    });
+                    wx.hideMenuItems({
+                        menuList: ['menuItem:favorite']
+                    });
+                });
+            }
+            else {
+                alert(data.message);
+            }
+        },
+        error: function () {
+
+        }
+    });
 },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
